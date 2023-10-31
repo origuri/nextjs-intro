9a9498e86ea5e1ab2721ccb07bc007c0bf98a1cd
origuri <dlcksrb7@naver.com>
2023-10-31 10:36:52 +0900

    2023.10.31 - hydration, pre rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import * as React from "react";
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  const handleCount = () => {
+    setCounter((prev) => prev + 1);
+  };
+
   return (
     <div>
-      <h1>hello</h1>
+      <h1>hello {counter}</h1>
+      <button onClick={handleCount}>click</button>
     </div>
   );
 }
---
+++ pages/ori.tsx
@@ -0,0 +1,7 @@
+export default function Guri() {
+  return (
+    <div>
+      <h1>ori!!</h1>
+    </div>
+  );
+}
